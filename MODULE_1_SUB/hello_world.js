c894d61ed7c7802bcf1156eae097eea792ac2589
Your Usernamelukeverongit config --global lukeverongit config --global user.name lukeveron <lukesveron@gmail.com>
2023-03-20 18:34:29 +1100

    Module 1 Submissions

---
+++ MODULE_1_SUB/hello_world.js
@@ -0,0 +1,40 @@
+{ let beach = ['Bondi','Tamarama', 'Maroubra', 'Cronulla', 'Manly', 'Wategoes', 'Bingin', 'Geger']
+console.log(Array.isArray(beach));
+
+let beach0 = {
+    name: 'Bondi Beach',
+    location: 'Sydney'
+}
+
+let beach1 = {
+    name: 'Tamarama Beach',
+    location: 'Sydney'
+}
+let beach2 = {
+    name: 'Maroubra Beach',
+    location: 'Sydney'
+}
+let beach3 = {
+    name: 'Cronulla Beach',
+    location: 'South Sydney'
+}
+let beach4 = {
+    name: 'Manly Beach',
+    location: 'North Sydney'
+}
+let beach5 = {
+    name: 'Wategoes Beach',
+    location: 'Byron Bay'
+}
+
+let beach6 = 
+    name: 'Bingin Beach',
+    location: 'Bali' }
+
+let beach7 = {
+    name: 'Geger Beach',
+    location: 'Indonesia'
+}
+
+console.log(beach0,beach2, beach3, beach5, beach6, beach7)
+}
